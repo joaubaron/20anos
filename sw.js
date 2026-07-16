0b18851f1588c2981667d4cce27dc7e6c32910c1
joaubaron <joaubaron@users.noreply.github.com>
2026-07-16 18:39:06 +0000

    deploy: 84407e16e3a84190baaa267d14d6fd07f334a4a4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker 
 // Toda vez que trocar fotos/áudios, a versão será atualizada automaticamente pelo deploy.yml
-const CACHE_VERSION = '16.07.2026-1537';
+const CACHE_VERSION = '16.07.2026-1539';
 const CACHE_NAME = `20anos-${CACHE_VERSION}`;
 
 const ASSETS = [
